42e673bc37507e13901018bb93d04ced38cb580d
Michael Golfi <Michael Golfi>
2017-01-15 15:16:03 -0500

    Added main

---
+++ main.py
@@ -0,0 +1,12 @@
+"""
+
+COMP 551 - Assignment 1
+Marathon Analysis
+
+Data taken from:
+https://www.athlinks.com/Events/383053/Courses/570971/
+
+"""
+
+# Authors: Michael Golfi <michael.golfi@mail.mcgill.ca>, Ralph Bou-Samra <ralph.bou-samra@mcgill.ca> Sneha Desai <sneha.desai@mail.mcgill.ca>
+# License: MIT
