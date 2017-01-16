503af69cc8a678cf25c6ed179950f41ae99e87bc
Michael Golfi <Michael Golfi>
2017-01-15 21:04:10 -0500

    Removed name col

--- main.py
+++ main.py
@@ -14,8 +14,20 @@ https://www.athlinks.com/Events/383053/Courses/570971/
 import pandas as pd
 import numpy as np
 
+def init_print():
+    pd.set_option('display.max_columns', None)
+    pd.set_option('display.max_rows', None)
+
+def print_groupby(grouped):
+    for key, item in grouped:
+        print key
+        print grouped.get_group(key), "\n"
+
+init_print()
+
 ageCategories = [11, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80]
 df = pd.read_csv('data/data.csv')
+del df["Name"]
 df["Age Category"] = pd.cut(df['Age Category'], 
     bins=ageCategories, 
     include_lowest=True, 
@@ -23,6 +35,4 @@ df["Age Category"] = pd.cut(df['Age Category'],
 
 grouped = df.groupby(['Age Category', 'Sex'])
 
-for name, group in grouped:
-   print(name)
-   print(group)
+print_groupby(grouped)
