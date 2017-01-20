8cd599114202ac6f6f6a8bbb8bfc650c59e8f8a7
Ralph <bousamra.ralph@gmail.com>
2017-01-20 18:38:06 -0500

    removed pace + converted HH:MM:SS to seconds

---
+++ convertTime.py
@@ -0,0 +1,17 @@
+"""
+Converts the given time from HH:MM:SS format to seconds
+"""
+
+import pandas as pd
+import numpy as np
+from pandas import DataFrame
+
+def get_sec(time_str):
+	h, m, s = time_str.split(':')
+	return int(h) * 3600 + int(m) * 60 + int(s)
+
+df = pd.read_csv('data/data.csv')
+#df['Time'].apply(get_sec, axis=1, broadcast=False, raw=False, reduce=None)
+df['Time'] = df['Time'].apply(get_sec)
+
+df.to_csv('data/data.csv')
