71b90a4ecaeb3ae0d0f4dfa768790d563c0a581c
Michael Golfi <Michael Golfi>
2017-01-26 18:52:58 -0500

    Added reigel. Added all feature combinations.

--- generate.py
+++ generate.py
@@ -41,7 +41,10 @@ df = pd.read_csv(FILENAME, usecols=COLS) \
     .not_equals(SEX, "U") \
     .convert_time() \
     .cut(AGE, AGE_BINS) \
-    .remove_duplicate_runners(COLS)
+    .remove_duplicate_runners(COLS) \
+    .reigel()
+
+print df[df[YEAR] == 2013]
 
 """ 
 Questions:
@@ -69,7 +72,7 @@ pivot = pivot[[SEX, AGE, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, R
 def addLastRace(pivotTable):
     lastYears = []
     for record in pivotTable.iterrows():
-        lastYear = 0
+        lastYear = 1
         
         # Start from 2015 down since we need to test for 2016
         for year in [2015, 2014, 2013, 2012, 2011, 2010, 2009, 2008]:
@@ -81,9 +84,9 @@ def addLastRace(pivotTable):
 
 print "Finding total races per participants and last race participated in"
 pivot["TotalRaces"] = pivot[[2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015]].sum(axis=1)
-pivot["TotalRaces"] = pivot["TotalRaces"] / pivot["TotalRaces"].max()
+pivot["TotalRaces"] = pivot["TotalRaces"]
 addLastRace(pivot)
-pivot["LastRace"] = pivot["LastRace"] / pivot["LastRace"].max()
+pivot["LastRace"] = pivot["LastRace"]
 
 print "Splitting data to train and test sets"
 cutoff = np.random.rand(len(pivot)) < TRAINING_THRESHOLD
@@ -96,9 +99,11 @@ participantsIn2016 = train[participated2016]
 nonParticipantsIn2016 = train[~participated2016]
 
 print "Generating Training Data"
-train[[SEX, AGE, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, TIME, RANK,  "TotalRaces", "LastRace"]].to_csv(OUTPUT_BASE + "training_x.csv")
-train[2016].to_csv(OUTPUT_BASE + "training_y.csv")
+#train[[SEX, AGE, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, TIME, RANK,  "TotalRaces", "LastRace"]].to_csv(OUTPUT_BASE + "training_x.csv")
+train[[AGE, 2015, "TotalRaces", RANK, "LastRace"]].to_csv(OUTPUT_BASE + "train_x.csv")
+train[2016].to_csv(OUTPUT_BASE + "train_y.csv")
 
 print "Generating Test Data"
-test[[SEX, AGE, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, TIME, RANK, "TotalRaces", "LastRace"]].to_csv(OUTPUT_BASE + "test_x.csv")
+#test[[SEX, AGE, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, TIME, RANK, "TotalRaces", "LastRace"]].to_csv(OUTPUT_BASE + "test_x.csv")
+test[[AGE, 2015, "TotalRaces", RANK, "LastRace"]].to_csv(OUTPUT_BASE + "test_x.csv")
 test[2016].to_csv(OUTPUT_BASE + "test_y.csv")
